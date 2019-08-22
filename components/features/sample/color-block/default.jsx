e37ecf6a72fe1e1b16baad0c5374fc92e5b503ba
RobinGiannattasio <robin.giannattasio@washpost.com>
2019-08-21 22:23:50 -0400

    Add custom field to enable user configuration

--- components/features/sample/color-block/default.jsx
+++ components/features/sample/color-block/default.jsx
@@ -1,27 +1,45 @@
 /*  /components/features/sample/color-block/default.jsx  */
 
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useComponentContext } from 'fusion:context';
 
 const ColorBlock = () => {
+  /* retrieve custom fields from fusion's component context */
+  const componentContext = useComponentContext();
+  const { hexCode } = componentContext.customFields;
+
   /* generate a random RGB color value */
-  let rgb = []
+  let rgb = [];
   for (var i = 0; i < 3; i++) {
     let r = Math.floor(Math.random() * 256)
     rgb.push(r)
   }
 
+  /* select background color */
+  const bgColor = hexCode ? `#${hexCode}` : `rgb(${rgb})`;
+
   /* set styles for color block */
   const styles = {
     height: '250px',
     margin: '10px',
-    backgroundColor: `rgb(${rgb})`,
+    backgroundColor: bgColor,
   };
 
   return (
     <div style={styles} />
-  )
+  );
 }
 
 ColorBlock.static = true;
 
-export default ColorBlock
+ColorBlock.propTypes = {
+  customFields: PropTypes.shape({
+    hexCode: PropTypes.string.tag({
+      label: 'Hex Code',
+      description: 'Please enter a valid color hex value. For example, 6b5d96.'
+    }),
+  }),
+};
+
+export default ColorBlock;
