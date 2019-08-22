ab4476e584bfde6f2d72fb62758cf74856405bf4
RobinGiannattasio <robin.giannattasio@washpost.com>
2019-08-21 23:41:06 -0400

    Add full width promo section to the basic layout

--- components/layouts/basic.jsx
+++ components/layouts/basic.jsx
@@ -1,30 +1,33 @@
 /*  /components/layouts/basic.jsx  */
 
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const BasicLayout = (props) => {
-  const [header, main, aside, footer] = props.children;
+  const [promo, header, main, aside, footer] = props.children;
   return (
-    <div className="container">
-      <div className="row">
-        <header className='col-lg-12'>
-          {header}
-        </header>
-        <main className='col-lg-8'>
-          {main}
-        </main>
-        <aside className='col-lg-4'>
-          {aside}
-        </aside>
-        <footer className='col-lg-12'>
-          {footer}
-          <p>Copyright &copy; 2019</p>
-        </footer>
+    <Fragment>
+      {promo}
+      <div className="container">
+        <div className="row">
+          <header className='col-lg-12'>
+            {header}
+          </header>
+          <main className='col-lg-8'>
+            {main}
+          </main>
+          <aside className='col-lg-4'>
+            {aside}
+          </aside>
+          <footer className='col-lg-12'>
+            {footer}
+            <p>Copyright &copy; 2019</p>
+          </footer>
+        </div>
       </div>
-    </div>
+    </Fragment>
   )
 }
 
-BasicLayout.sections = ['header', 'main', 'aside', 'footer']
+BasicLayout.sections = ['promo', 'header', 'main', 'aside', 'footer']
 
 export default BasicLayout
