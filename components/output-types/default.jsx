23b8c23441de10238a4b1b003912477fe92036cd
RobinGiannattasio <robin.giannattasio@washpost.com>
2019-08-21 20:17:42 -0400

    Modify the output type to use bootstrap

--- components/output-types/default.jsx
+++ components/output-types/default.jsx
@@ -13,13 +13,15 @@ export default ({
 }) =>
   <html>
     <head>
-      <title>Fusion Article</title>
+      <title>Fusion Article</title> 
       <MetaTags />
       <Libs />
       <CssLinks />
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
       <link rel='icon' type='image/x-icon' href={deployment(`${contextPath}/resources/favicon.ico`)} />
     </head>
     <body>
+      <h2 class="text-info">Welcome to Fusion!</h2>
       <div id='fusion-app'>
         {children}
       </div>
