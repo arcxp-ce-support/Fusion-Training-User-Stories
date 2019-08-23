511deca45ac9f6fd5b9f5c126db96f5c36acfff9
RobinGiannattasio <robin.giannattasio@washpost.com>
2019-08-22 22:07:50 -0400

    Add inline editing to the alert bar

--- components/features/global/alert-bar/default.jsx
+++ components/features/global/alert-bar/default.jsx
@@ -1,8 +1,9 @@
 /*  /components/features/article/alert-bar/default.jsx  */
 
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { useComponentContext } from 'fusion:context';
+import { useEditableContent } from 'fusion:content';
 
 const AlertBar = () => {
   /* retrieve custom fields from fusion's component context */
@@ -10,18 +11,22 @@ const AlertBar = () => {
   const {
     alertType,
     link,
-    linkText,
     text,
   } = componentContext.customFields;
 
+  /* get inline editing function from fusion's content */
+  const { editableField } = useEditableContent();
+
   return (
-    <section className={`alert alert-${alertType} w-100`} role='alert'>
-      {text}
-      {
-        linkText
-        &&
-        <Fragment>: <a href={link} className='alert-link'>{linkText}</a></Fragment>
-      }
+    <section className={`alert alert-${alertType} w-100`} role='alert' >
+      <a
+        {...editableField('text')}
+        suppressContentEditableWarning
+        href={link}
+        className='alert-link'
+      >
+        {text}
+      </a>
     </section>
   );
 }
@@ -42,17 +47,14 @@ AlertBar.propTypes = {
       name: 'Alert Type',
     }),
     text: PropTypes.string.tag({
-      name: 'Main Alert Text'
-    }).isRequired,
-    linkText: PropTypes.string.tag({
       group: 'Call to Action',
       name: 'Text',
       description: 'Text to prompt the users to click the link',
-    }),
+    }).isRequired,
     link: PropTypes.url.tag({
       group: 'Call to Action',
       name: 'Url',
-    }),
+    }).isRequired,
   }),
 };
 
