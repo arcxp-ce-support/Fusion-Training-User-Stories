08482664a0a4b0a417e3af8820b88ae070c993b3
RobinGiannattasio <robin.giannattasio@washpost.com>
2019-08-22 23:13:04 -0400

    Refactor card to fetch component level content

--- components/features/global/card/default.jsx
+++ components/features/global/card/default.jsx
@@ -1,21 +1,46 @@
 /*  /components/features/global/card/default.jsx  */
 
 import React from 'react';
-import { useAppContext } from 'fusion:context';
+import PropTypes from 'prop-types';
+import { useContent } from 'fusion:content';
 
-const Card = () => {
-  /* retrieve global content from fusion's app context */
-  const appContext = useAppContext();
-  const { globalContent } = appContext;
+const Card = (props) => {
+  /*
+    Retrieve content config values from custom fields
+    Notice that here we are using props directly instead of component context
+  */
+  const {
+    customFields: {
+      storyConfig = {},
+    },
+  } = props;
+  const { contentService, contentConfigValues } = storyConfig;
+
+  /* fetch component content using Fusion's useContent */
+  const story = useContent({
+    source: contentService,
+    query: contentConfigValues,
+  });
+
+  /* prevent the story card from rendering if there is no content */
+  if (!story) return null;
 
   return (
     <article className="card">
       <div className="card-body">
-        <h2 className="card-title">{globalContent.headlines.basic}</h2>
-        <h5 className="card-text">{globalContent.subheadlines.basic}</h5>
+        <h2 className="card-title">{story.headlines.basic}</h2>
+        <h5 className="card-text">{story.subheadlines.basic}</h5>
       </div>
     </article>
   );
 }
+
+Card.propTypes = {
+  customFields: PropTypes.shape({
+    storyConfig: PropTypes.contentConfig('article').tag({
+      label: 'Story URL',
+    }).isRequired,
+  }),
+};
 
 export default Card;
