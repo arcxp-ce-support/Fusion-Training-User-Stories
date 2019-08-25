b89266c1da8afc4d5f2b4b4afa49bef70b8eb7c6
RobinGiannattasio <robin.giannattasio@washpost.com>
2019-08-25 16:56:58 -0400

    Add amp output type

---
+++ components/output-types/amp.jsx
@@ -0,0 +1,31 @@
+'use strict'
+
+import React from 'react'
+
+export default ({
+  children,
+  contextPath,
+  deployment,
+  CssLinks,
+  Fusion,
+  Libs,
+  MetaTags
+}) =>
+  <html>
+    <head>
+      <title>Fusion Article</title>
+      <MetaTags />
+      <Libs />
+      <CssLinks />
+      <link rel='icon' type='image/x-icon' href={deployment(`${contextPath}/resources/favicon.ico`)} />
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+      <script async src="https://cdn.ampproject.org/v0.js"></script>
+    </head>
+    <body>
+      <div id='fusion-app'>
+        {children}
+      </div>
+      <Fusion />
+    </body>
+  </html>
+ 
