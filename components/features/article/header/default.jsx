832076d68366ea17ba095ab52196b8681c887f45
RobinGiannattasio <robin.giannattasio@washpost.com>
2019-08-25 16:45:47 -0400

    Use site properties in article header

--- components/features/article/header/default.jsx
+++ components/features/article/header/default.jsx
@@ -4,13 +4,13 @@ import React from 'react';
 import { useAppContext } from 'fusion:context';
 
 const Header = () => {
-  /* retrieve global content from fusion's app context */
+  /* retrieve global content and site properties from fusion's app context */
   const appContext = useAppContext();
-  const { globalContent } = appContext;
+  const { globalContent, siteProperties } = appContext;
 
   return (
     <header className="container-fluid">
-      <span className="text-info">News from Demo!</span>
+      <span className="text-info">{`News from ${siteProperties.defaultSiteTitle}!`}</span>
       <h2 className="card-title">{globalContent.headlines.basic}</h2>
       <h5 className="card-text">{globalContent.subheadlines.basic}</h5>
     </header>
