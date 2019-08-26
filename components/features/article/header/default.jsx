de192738f0aeac30f2be82f1c52f67e76ea57eed
RobinGiannattasio <robin.giannattasio@washpost.com>
2019-08-25 21:19:19 -0400

    Use output type to as a flag to render the kicker

--- components/features/article/header/default.jsx
+++ components/features/article/header/default.jsx
@@ -6,11 +6,18 @@ import { useAppContext } from 'fusion:context';
 const Header = () => {
   /* retrieve global content and site properties from fusion's app context */
   const appContext = useAppContext();
-  const { globalContent, siteProperties } = appContext;
+  const {
+    globalContent,
+    outputType,
+    siteProperties,
+  } = appContext;
 
   return (
     <header className="container-fluid">
-      <span className="text-info">{`News from ${siteProperties.defaultSiteTitle}!`}</span>
+      {
+        outputType !== 'amp' &&
+        <span className="text-info">{`News from ${siteProperties.defaultSiteTitle}!`}</span>
+      }
       <h2 className="card-title">{globalContent.headlines.basic}</h2>
       <h5 className="card-text">{globalContent.subheadlines.basic}</h5>
     </header>
