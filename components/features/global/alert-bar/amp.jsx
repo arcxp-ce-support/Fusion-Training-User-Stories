dd2d482c2efefba55e1534da8686c720a685a0ce
RobinGiannattasio <robin.giannattasio@washpost.com>
2019-08-25 21:19:55 -0400

    Prevent the alert bar from rendering in amp

---
+++ components/features/global/alert-bar/amp.jsx
@@ -0,0 +1,8 @@
+/*  /components/features/article/alert-bar/default.jsx  */
+
+// do not render an amp component in the default output type
+const AlertBar = () => {
+  return null;
+}
+
+export default AlertBar;
